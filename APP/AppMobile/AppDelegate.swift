fc60366a21b8c69db19554cfe4f7c3018a027d79
EldaReqica <121405504+EldaReqica@users.noreply.github.com>
2024-10-18 22:32:30 +0200

    Update AppDelegate.swift

--- APP/AppMobile/AppDelegate.swift
+++ APP/AppMobile/AppDelegate.swift
@@ -2,7 +2,7 @@
 //  AppDelegate.swift
 //  AppMobile
 //
-//  Created by Elda Reçica on 2/20/24.
+//  Created by Elda Reçica on 10/18/24.
 //  Copyright © 2024 Elda Reçica. All rights reserved.
 //
 
